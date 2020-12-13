a76ebd0f626c99fb2793ea850935fad9e9ed9b39
Artur Wenc <artur.wenc.dev@gmail.com>
2020-12-13 17:06:56 +0000

    removing second sentence from analyisis output

--- code/extractor.py
+++ code/extractor.py
@@ -3,9 +3,8 @@ from sentence_components import sent_, get_sent, n_chunk, noun_chunks_sent, text
 
 
 def main():
-    print(n_chunk(get_sent(text)[1]))
-
-    noun_chunks_sent(get_sent(text)[0])
+    print(n_chunk(get_sent(text)[0]))
+    # noun_chunks_sent(get_sent(text)[0])
 
     for sentence in get_sent(text):
         print(sentence)
