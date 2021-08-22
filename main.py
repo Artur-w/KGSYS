2e762a2e7e3751da2adad2bd88a089953bd319e3
Artur Wenc <artur.wenc.dev@gmail.com>
2021-08-22 02:00:59 +0100

    refactoring

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ from Triple import knowledge_graph
 from Triple import Triple
 from tqdm import tqdm
 import pandas as pd
-
-# Read in data
-path_to_csv = "data/csv/sentences_psychology.csv"
+import os.path
+# Read in data into dataframe
+path_to_csv = input("Enter path to csv file")
+path_to_csv = str(path_to_csv)
+# "data/csv/sentences_psychology.csv"
 df = pd.read_csv(path_to_csv)
-print(f"Psychology data corpus {len(df['sentence'])} sentences")
-
-def _test():
-    percy = "Percy the mockingbird spent the whole warm season chirping and twittering"
-    Triple(percy).tree('dep_tree')
-    Triple(percy).graph('dep')
+print(f"Data corpus {len(df['sentence'])} sentences")
+file_name = os.path.split(path_to_csv)[1][:-4]
+# def _test():
+#     percy = "Percy the mockingbird spent the whole warm season chirping and twittering"
+#     Triple(percy).tree('dep_tree')
+#     Triple(percy).graph('dep')
 
 
 def main():
@@ -23,16 +25,14 @@ def main():
     for i, sent in enumerate(tqdm(df['sentence'])):
         triples.append(Triple(sent).get_triple())
 
-        # Graphs (uncomment to save artefacts on disk)
+        # # Graphs (uncomment to save artefacts on disk)
         # Triple(sent).tree('tree1'+str(i))
         # Triple(sent).graph('tree1'+str(i))
 
-    # knowledge_graph(triples)
-
     df['triples'] = triples
-    df.to_csv('data/csv/psychology_data.csv')
+    df.to_csv('data/out/'+file_name+'.csv')
 
-    knowledge_graph(list(df['triples'].head()))
+    knowledge_graph(list(df['triples'].tail()))
     print("DONE!")
 
 
